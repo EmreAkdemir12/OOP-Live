6858e831a62586601a5ee4e43794aaaa7bc0f87f
Emre Akdemir <147593636+EmreAkdemir12@users.noreply.github.com>
2024-01-02 02:39:45 +0300

    Add files via upload

---
+++ clsStudent.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Classes
+{
+    internal class clsStudent
+    {
+        // Sınıfın kimliğini hazırladık bir anlamda...
+        public string OgrAd; // dışa açık personelin adı
+        public string OgrSoyad; // dışa açık personelin soyadı
+        public string OgrSinif;
+        public byte Not1;
+        public byte Not2;
+        public byte Ortalama;
+
+        public void setInfos()
+        {
+
+        }
+
+        public int getStatus()
+        {
+            return 0;
+        }
+
+    }
+}
